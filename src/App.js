5eb54414dfd96248fcb81a64c67ae0cdd17e345e
DeborahIhesiaba <deborahihesiaba@gmail.com>
2023-06-28 13:50:51 +0200

    Added key prop

--- src/App.js
+++ src/App.js
@@ -72,6 +72,7 @@ const App = () => {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
